d5bef25ab0ca98ef496594cd3e6ea6dbf888b501
Antonyos milad fahim necola <antonyosmilad60@gmail.com>
2025-12-01 21:06:59 +0200

    delete client

--- Client/src/Pages/Levels.tsx
+++
@@ -1,7 +0,0 @@
-export default function Levels() {
-  return (
-    <div>
-      <h1>Welcome to Levels!</h1>
-    </div>
-  )
-}
--- Client/src/Pages/MyProgress.tsx
+++
@@ -1,7 +0,0 @@
-export default function MyProgress() {
-  return (
-    <div>
-      <h1>Welcome to My Progress!</h1>
-    </div>
-  )
-}
