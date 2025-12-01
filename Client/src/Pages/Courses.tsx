6d4ff8f9fd1c889e053e385ea954c57258ca9a26
Antoine sobhy soliman <sobhyantoine@gmail.com>
2025-12-01 22:59:51 +0200

    adding headers for courses section

--- Client/src/Pages/Courses.tsx
+++ Client/src/Pages/Courses.tsx
@@ -56,7 +56,9 @@ export default function Courses() {
   ];
 
   return (
-    
+    <>
+    <h2 className="heading">All Courses</h2>
+    <p className="sub-heading pb-2">Browse and manage your courses across all levels</p>
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 justify-items-center sm:justify-items-start">
       
       {coursesData.map((course) => (
@@ -73,5 +75,6 @@ export default function Courses() {
       ))}
 
     </div>
+    </>
   );
 }
